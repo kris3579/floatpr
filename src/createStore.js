ec1855a891b97e92c467c6b9a1567a4a3334aee0
Kristian Esvelt <kristianesvelt@hotmail.com>
2019-10-17 22:49:35 -0700

    deployment edits

--- src/createStore.js
+++ src/createStore.js
@@ -1,8 +1,7 @@
 import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import mainReducer from './reducers/mainReducer';
 
 export default () => {
-  return createStore(mainReducer, composeWithDevTools());
+  return createStore(mainReducer);
 };
